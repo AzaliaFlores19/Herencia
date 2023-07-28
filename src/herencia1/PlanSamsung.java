6290eb0e31835d90c9ff72ecfbc63ee5e625b818
Reyesalv20 <124184473+Reyesalv20@users.noreply.github.com>
2023-07-28 15:49:11 -0600

    cambios

---
+++ src/herencia1/PlanSamsung.java
@@ -0,0 +1,78 @@
+
+package herencia1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ruth Reyes
+ */
+public abstract class PlanSamsung extends Plan {
+    
+    //Atributos
+    public String Pin,pinamig;
+ ArrayList<Plan>bbm;
+    public String email;
+
+
+    
+    
+    public PlanSamsung(int numero, String nombre,String pin,int minutos, int mensajes){ 
+        
+     super(numero,nombre,minutos,mensajes);
+       Pin = pin;
+       bbm = new ArrayList<>();
+        
+    }
+
+    @Override
+    public double pagoMensual(int minutos, int mensajes) {
+      
+        double costoMinAd=0.8,costoMsgAd=0.2,cobro= 40.0,totalMinutos,totalMensajes;
+        int mingratis = 200,msgsgratis= 300;
+
+int minConsumidos = this.getminutos();
+int menConsumidos = this.getmensajes() ;
+
+// Cálculo de minutos adicionales
+int minAdicionales;
+if (minConsumidos > mingratis) {
+    minAdicionales = minConsumidos - mingratis;
+} else {
+    minAdicionales = 0;
+}
+
+// Cálculo de mensajes adicionales
+int menAdicionales;
+if (menConsumidos > msgsgratis) {
+    menAdicionales = menConsumidos - msgsgratis;
+} else {
+    menAdicionales = 0;
+}      
+      
+ totalMinutos = costoMinAd * mingratis;
+ totalMensajes = costoMsgAd * menAdicionales;
+      
+        
+        
+  return cobro + totalMinutos + totalMensajes;
+      
+    }
+
+public void agregarPinAmigo(String pinamig) {
+
+    
+    for(int i=0;i<bbm.size();i++){
+    if (bbm.get(i).equals(pinamig)) {
+        
+        //bbm.add(pinamig);
+         
+        }
+    }
+}
+    
+     public String getEmail() {
+        return email;
+    }
+      
+}
